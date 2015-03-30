bd1db56b3875c6f33614835d9be3147eb657b078
Greg Shapiro <gshapiro@ngpvan.com>
2015-03-30 11:42:55 -0400

    GetStarted EF and MVC - adding a schoolcontext dbcontext

---
+++ GettingStartedEFandMVC/GetStartedEF/GetStartedEF/DAL/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿using GregshapUniversity.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace GregshapUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext()
+            : base("SchoolContext")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            //in VAN we actually like plural table names. 
+            //Not sure what oberon does, or what we want for extracted apps?
+            //Leaving the override here for now so its singular as in the tutorial
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
